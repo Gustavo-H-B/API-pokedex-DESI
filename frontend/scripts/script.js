7d6da44d4e6dfca92c27005136db5888fe401fb2
Gustavo-H-B <nelsonbruesky@gmail.com>
2026-09-10 21:42:58 -0300

    feito bastante coisa...

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,4 +1,31 @@
-const codigo = document.getElementById("codigo");
+const comecar = document.getElementById("comecar");
+let todosPokemons = [];
 
-codigo.addEventListener("", () => {
+comecar.addEventListener("click", async () => {
+    try {
+        const response = await fetch ("https://pokeapi.co/api/v2/pokemon?limit=69");
+        const dados = await response.json();
+
+        todosPokemons = await Promise.all(
+            dados.results.map(async (pokemon) => {
+                const response = await fetch(pokemon.url);
+                return await response.json();
+            })
+        );
+
+        infosPokemon();
+    } catch (error) {
+        console.log(error);
+    }; 
 });
+
+function infosPokemon() {
+        todosPokemons.forEach(pokemon => {
+            appeared.innerHTML += `
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <h5 class="modal-tittle">${pokemon.name}</h5>
+                    <img src="${pokemon.sprites.other["official-artwork"].front_default}">
+                </div>
+            `;
+        });
+    }; 
